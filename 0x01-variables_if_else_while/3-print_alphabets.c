8374aa650e3daa0d57b34c6bf3d35f6e1c329f93
Tashydaije <mbuguanatasha01@gmail.com>
2023-04-24 12:47:49 -0700

    correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,13 +14,13 @@ int main(void)
 	int lower = 97;
 	int upper = 65;
 
-	while(lower <= 122)
+	while (lower <= 122)
 	{
 		putchar(lower);
 		lower++;
 	}
 
-	while(upper <= 90)
+	while (upper <= 90)
 	{
 		putchar(upper);
 		upper++;
