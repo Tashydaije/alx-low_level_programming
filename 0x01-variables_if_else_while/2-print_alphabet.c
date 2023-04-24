6bc2fe50ef9b3bfb3db9a972e3bb01a4c951340f
Tashydaije <mbuguanatasha01@gmail.com>
2023-04-24 11:08:37 -0700

    create: 2-print_alphabet.c and compile

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*declare variable*/ 
+	char ch;
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		/*print value of ch*/
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
