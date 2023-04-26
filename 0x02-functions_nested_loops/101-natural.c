4e7ef53b80199f32fa64eead4d5880ec40db6fcb
Tashydaije <mbuguanatasha01@gmail.com>
2023-04-26 04:53:00 -0700

    create: 101-natural.c file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - List all natural numbers below 1024
+ * that are multiples of 3 and 5
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	int x = 0;
+	int sum = 0;
+
+	while (x < 1024)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			sum += x;
+		}
+	}
+	x++;
+	printf("%d\n ", sum);
+	return (0);
+}
