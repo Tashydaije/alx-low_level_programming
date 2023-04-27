d5a6608e46c56eeb114fb1a4af0383931b0dd866
Tashydaije <mbuguanatasha01@gmail.com>
2023-04-27 13:45:11 -0700

    add: func calculating product of 2 variables

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - calc product of a and b
+ * @a: int variable
+ * @b: int variable
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
