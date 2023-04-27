5f579f3d776757ecf42fddccba3a190a4ba84c06
Tashydaije <mbuguanatasha01@gmail.com>
2023-04-27 13:56:09 -0700

    create: 3-print_numbers.c file, func that prints no. using _putchar.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by new line
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c = 0;
+
+	while (c <= 9)
+	{
+		_putchar(c + '0');
+		c++;
+	}
+	
+	_putchar('\n');
+
+}
