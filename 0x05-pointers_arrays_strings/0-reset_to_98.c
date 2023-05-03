01ad9327e1861db6855c8e501f2389e46124722d
Tashydaije <mbuguanatasha01@gmail.com>
2023-05-03 09:01:47 -0700

    func that updates val of n

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - updates val of n to 98
+ * @n: integer pointer to a var to be updates
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
