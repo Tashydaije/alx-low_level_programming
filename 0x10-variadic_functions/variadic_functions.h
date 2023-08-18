c696de88352b442d8733b05ee016507bc77e8e5b
Tashydaije <mbuguanatasha01@gmail.com>
2023-08-18 16:21:49 -0700

    prototypes and struct for task3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,28 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct printer - new struct type defining a printer
+ * @symbol: symbol representing a data type
+ * @print - func pointer to funcs that take a va_list arg as a para.
+ */
+
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 
 #endif
