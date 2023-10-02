dd884c30d9a86528925ba2b69d044ee36c656371
Tashydaije <mbuguanatasha01@gmail.com>
2023-10-02 13:57:50 -0700

    task0: reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it
+ *                 to the POSIX standard output.
+ * @filename: A pointer to the text file.
+ * @letters: The number of letters to read n print
+ *
+ * Return: If filename is NULL or the file cannot be opened or read - 0.
+ *         If write fails or does not write the expected number of bytes - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY, 0400);
+	if (o == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	r = read(o, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	w = write(1, buffer, r);
+	if (w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
