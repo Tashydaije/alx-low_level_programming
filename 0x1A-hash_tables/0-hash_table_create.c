3893417cb0981b9c203dff171c2ce440fe3fc2c4
Tashydaije <mbuguanatasha01@gmail.com>
2023-12-01 18:05:43 -0800

    Initial commit with task 0 - 2

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table->array);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
