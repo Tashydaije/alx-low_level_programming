c32d92baa85816ece7dde8fc5b87aba691f65788
Tashydaije <mbuguanatasha01@gmail.com>
2023-12-01 18:59:58 -0800

    task6: delete ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes the hash table
+ * @ht: the hash table
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+
+	free(ht);
+}
